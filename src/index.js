cfa5ac3bc753e0f4cf5e153532a134115589bd81
Emmanuel Uchenna <eunitwap@gmail.com>
2020-09-03 19:03:16 +0100

    Inititial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+
+
+var where = document.querySelector("#where")
+
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  where
+);
